7ca527b7aa08c9907037a43965ab6a16cb864398
rocha <rocha7778@hotmail.com>
2016-01-19 08:53:05 -0500

    se agrega jumbotron

--- peticion/slide.php
+++ peticion/slide.php
@@ -10,18 +10,25 @@ $token = $cp->captchat('loging');
     <head>
 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-56941141-1', 'auto');
-  ga('send', 'pageview');
+            ga('create', 'UA-56941141-1', 'auto');
+            ga('send', 'pageview');
 
-</script>
+        </script>
 
-<meta name="description" content="Gestionprox es un software  para la gestión de la producción  y manejo óptimo de la información. Su principal objetivo es eliminar la papelería en el área de producción y servir como herramienta para los planes de mantenimiento,  análisis  y eliminación de los  tiempos improductivos." />
+        <meta name="description" content="Gestionprox es un software  para la gestión de la producción  y manejo óptimo de la información. Su principal objetivo es eliminar la papelería en el área de producción y servir como herramienta para los planes de mantenimiento,  análisis  y eliminación de los  tiempos improductivos." />
 
 
     </head>
@@ -48,26 +55,36 @@ $token = $cp->captchat('loging');
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="\"">GestionProx <small>Software para la Gestion de la Producci&oacute;n</small></a>
-                   </div>
-                 
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <div class="container" >
+
+        <div class="page-header">
+            <div class="container">
+                <h1>GestionProx <small>Software para la Gestion de la Producci&oacute;n</small></h1>
+
+
+
+            </div>
         </div>
     </div>
     
-    
-     <div class="container" >
+    <div class="container">
+           
+                <div class="jumbotron">
+                <p>GESTIONPROX</p>
 
-            <div class="page-header">
-                <div class="container">
-                    <h1>GestionProx <small>Software para la Gestion de la Producci&oacute;n</small></h1>
-
-
-
-                </div>
             </div>
         </div>
-    
 
-    
+
+
 
     <div class="container">
         <!-- Example row of columns -->
@@ -97,10 +114,10 @@ $token = $cp->captchat('loging');
 
         <hr>
 
-<div class="container">
+        <div class="container">
             <!-- Example row of columns -->
             <div class="row">
-                
+
                 <div class="col-md-4">
                     <h2>Que es GestionProx</h2>
                     <p class="text-justify"> <b>Gestionprox</b> es un software  para la gesti&oacute;n de la producci&oacute;n  y manejo &oacute;ptimo de la informaci&oacute;n. 
@@ -112,30 +129,30 @@ $token = $cp->captchat('loging');
                 <div class="col-md-4">
                     <h2>Caracter&iacute;sticas</h2>
                     <p class="text-justify">  
-                        <ul>
-                             <li>  <p class="text-justify"> <b>Sencillo.</b> <strong>GestionProx</strong> Esta desarrollado pensando en la comodidad de sus usuarios, eliminando las complejidades que dificultan el uso de las herramientas inform&aacute;ticas. </p></li>
-                             <li> <b>Potente</b> <p class="text-justify"> <strong>GestionProx</strong> Procesa toda su informaci&oacute;n generando estad&iacute;sticos sencillo, facilitando la toma de decisiones en su dependencia</p></li>
-                             
-                        </ul>
-                </p>
+                    <ul>
+                        <li>  <p class="text-justify"> <b>Sencillo.</b> <strong>GestionProx</strong> Esta desarrollado pensando en la comodidad de sus usuarios, eliminando las complejidades que dificultan el uso de las herramientas inform&aacute;ticas. </p></li>
+                        <li> <b>Potente</b> <p class="text-justify"> <strong>GestionProx</strong> Procesa toda su informaci&oacute;n generando estad&iacute;sticos sencillo, facilitando la toma de decisiones en su dependencia</p></li>
+
+                    </ul>
+                    </p>
 
                 </div>
 
 
-                 <div class="col-md-4">
+                <div class="col-md-4">
                     <h2>Beneficios</h2>
                     <p class="text-justify">  
-                        <ul>
-                             <li>  <p class="text-justify"> <b>No gaste m&aacute;s papeler&iacute;a.</b> <strong>GestionProx</strong> Esta desarrollado para que reemplace el papel y comience  a disfrutar de los beneficios de la tecnolog&iacute;a en producci&oacute;n </p></li>
-                             <li> <b>Lib&eacute;rese</b> <p class="text-justify"> <strong>GestionProx</strong> Ofrece su versi&oacute;n m&oacute;vil totalmente gratis, para que gestione su empresa  donde quiera que se encuentre, liber&aacute;ndolo del espacio de la oficina o el uso exclusivo del computador</p></li>
-                             
-                        </ul>
-                </p>
+                    <ul>
+                        <li>  <p class="text-justify"> <b>No gaste m&aacute;s papeler&iacute;a.</b> <strong>GestionProx</strong> Esta desarrollado para que reemplace el papel y comience  a disfrutar de los beneficios de la tecnolog&iacute;a en producci&oacute;n </p></li>
+                        <li> <b>Lib&eacute;rese</b> <p class="text-justify"> <strong>GestionProx</strong> Ofrece su versi&oacute;n m&oacute;vil totalmente gratis, para que gestione su empresa  donde quiera que se encuentre, liber&aacute;ndolo del espacio de la oficina o el uso exclusivo del computador</p></li>
+
+                    </ul>
+                    </p>
 
                 </div>
-                
-                
-                
+
+
+
 
             </div>
 
@@ -145,7 +162,7 @@ $token = $cp->captchat('loging');
 
 
 
-           
+
         </div> <!-- /container -->
 
 
@@ -153,86 +170,88 @@ $token = $cp->captchat('loging');
         <div class="container">
             <!-- Example row of columns -->
             <div class="row">
-                
+
                 <div class="col-md-4">
                     <h2>Compatibildad</h2>
                     <p class="text-justify"><b>Gestionprox</b> est&aacute; desarrollado en lenguaje web lo que garantiza su  compatibilidad  con otros sistemas operativos distintos de Windows. </p>
 
                 </div>
-                
+
                 <div class="col-md-4">
-                     
-                     <h2>Adaptabilidad</h2>
+
+                    <h2>Adaptabilidad</h2>
                     <p class="text-justify">Gracias a su  entorno web <b> Gestionprox</b> puede ser utilizado en distintos dispositivos m&oacute;viles <br>
                         <a class="btn btn-success"> ver </a>
-                       
-                    
+
+
                     </p>
-                    
+
 
                 </div>
-                 <div class="col-md-4">
-                     
-                     <h2>Versatilidad</h2>
-                     <p class="text-justify"> Muchas  empresas   utilizan formatos en papel para registrar la informaci&oacute;n y luego esta ser cargada a un programa.
-                      <b>Gestionprox</b> brinda oportunidad para cargar los datos directamente en la aplicaci&oacute;n utilizando dispositivos m&oacute;viles de bajo costo </p>
-                     <a class="btn btn-info"> info </a>
-                    
+                <div class="col-md-4">
+
+                    <h2>Versatilidad</h2>
+                    <p class="text-justify"> Muchas  empresas   utilizan formatos en papel para registrar la informaci&oacute;n y luego esta ser cargada a un programa.
+                        <b>Gestionprox</b> brinda oportunidad para cargar los datos directamente en la aplicaci&oacute;n utilizando dispositivos m&oacute;viles de bajo costo </p>
+                    <a class="btn btn-info"> info </a>
+
 
                 </div>
 
 
 
-                
+
 
             </div>
+
+
+
+
+
+
+
+
+        </div> <!-- /container -->
+
+        <div class="container">
+
+            <div class="col-md-4">
+
+                <h2>Pago en linea</h2>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target ="_blank">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="business" value="rocha7778@hotmail.com">
+                    <input type="hidden" name="lc" value="ES">
+                    <input type="hidden" name="item_name" value="SERVICIO GESTIONPROX">
+                    <input type="hidden" name="button_subtype" value="services">
+                    <input type="hidden" name="no_note" value="0">
+                    <input type="hidden" name="currency_code" value="USD">
+                    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+                    <input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/es_ES/i/scr/pixel.gif" width="1" height="1">
+                </form>
+
+
+            </div>
+
+            <div class="col-md-4">
+
+                <h2>Contactenos</h2>
+                <p class="text-justify">
+                    Ventas<br>
+                    GestionProx<br>
+                    CEL. 304-4705467<br>
+                    E-mail: <a href="">servicioalcliente@gestionprox.com</a>
+                </p>
+
+
+            </div>
+
+        </div> <!-- contactenos -->
 
         
 
 
-
-
-
-           
-        </div> <!-- /container -->
-
-<div class="container">
-
-<div class="col-md-4">
-                     
-                     <h2>Pago en linea</h2>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target ="_blank">
-<input type="hidden" name="cmd" value="_xclick">
-<input type="hidden" name="business" value="rocha7778@hotmail.com">
-<input type="hidden" name="lc" value="ES">
-<input type="hidden" name="item_name" value="SERVICIO GESTIONPROX">
-<input type="hidden" name="button_subtype" value="services">
-<input type="hidden" name="no_note" value="0">
-<input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-<input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
-<img alt="" border="0" src="https://www.paypalobjects.com/es_ES/i/scr/pixel.gif" width="1" height="1">
-</form>
-                    
-
-                </div>
-
- <div class="col-md-4">
-                     
-                     <h2>Contactenos</h2>
-                     <p class="text-justify">
-                      Ventas<br>
-GestionProx<br>
-CEL. 304-4705467<br>
-E-mail: <a href="">servicioalcliente@gestionprox.com</a>
- </p>
-                    
-
-                </div>
-
-</div> <!-- contactenos -->
-
-   
 
         <footer>
             <p>&copy; Company 2014</p>
@@ -258,6 +277,6 @@ E-mail: <a href="">servicioalcliente@gestionprox.com</a>
     <!-- Latest compiled and minified JavaScript -->
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-    
+
 </body>
 </html>
